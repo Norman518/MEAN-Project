57d7f297b1c7c40b7ee2d621e57e7dc3ded6324e
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-08-30 08:50:11 -0400

    fetching data from the database

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,6 @@ const Post = require("./models/post");
 
 const app = express();
 const uri = "";
-
 mongoose
   .connect(uri, { useNewUrlParser: true })
   .then(() => {
@@ -38,28 +37,18 @@ app.post("/api/posts", (req, res, next) => {
     title,
     content,
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: "Post added successfully",
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server",
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts,
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents,
+    });
   });
 });
 
