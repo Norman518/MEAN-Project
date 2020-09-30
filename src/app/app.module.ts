d6b99780dbe9848123abbe100595a2d046db3968
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-09-30 10:50:58 -0400

    added paginator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { HttpClientModule } from '@angular/common/http';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent, HeaderComponent, PostListComponent],
   imports: [
+    MatPaginatorModule,
     ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
