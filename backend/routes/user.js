7340d4ebcbbe5b815b975a3344a16f703e6ab20e
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-10-08 10:05:00 -0400

    new user created upon request

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,21 @@
 const express = require("express");
-
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 const router = express.Router();
 
-router.post("/signup", (req, res, next) => {});
+router.post("/signup", (req, res, next) => {
+  const { email, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({ email, hash });
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({ message: "User created!", result });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  });
+});
 
 module.exports = router;
