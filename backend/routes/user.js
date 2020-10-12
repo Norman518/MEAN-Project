4bb58f2d43c92a9028c53d912ba108cb4dc586e7
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-10-12 08:56:32 -0400

    added SPA authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const user = require("../models/user");
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
@@ -15,15 +17,39 @@ router.post("/signup", (req, res, next) => {
       .then((result) => {
         res.status(201).json({
           message: "User created!",
-          result: result,
+          result,
         });
       })
-      .catch((err) => {
+      .catch((error) => {
         res.status(500).json({
-          error: err,
+          error,
         });
       });
   });
+});
+
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ message: "Auth failed" });
+      }
+      return bcrypt.compare(password, user.password);
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(401).json({ message: "Auth failed" });
+      }
+      const token = jwt.sign(
+        { email: user.email, userId: user._id },
+        "STrsST3!$%Jaf3@",
+        { expiresIn: "1h" }
+      );
+    })
+    .catch(() => {
+      return res.status(401).json({ message: "Auth failed" });
+    });
 });
 
 module.exports = router;
