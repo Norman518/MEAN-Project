c138869a24568b7b41b4bbd905d1cf85af8c352e
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-10-16 10:24:34 -0400

    improved ui to reflect authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -24,7 +30,7 @@ export class AuthService {
     this.http.post<{ token: string }>('http://localhost:3000/api/user/login', authData).subscribe((response) => {
       const token = response.token;
       this.token = token;
-      console.log('User has logged in.');
+      this.authStatusListener.next(true);
     });
   }
 }
