6b2019c193c6425539555d72767eaad70e9881f6
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-10-29 05:01:49 -0400

    protecting resources with authorization

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -71,8 +71,15 @@ router.put(
       content,
       imagePath: path,
     });
-    Post.updateOne({ _id: req.params.id }, post).then((result) => {
-      res.status(200).json({ message: "Update successful!" });
+    Post.updateOne(
+      { _id: req.params.id, creator: req.userData.userId },
+      post
+    ).then((result) => {
+      if (result.nModified > 0) {
+        res.status(200).json({ message: "Update successful!" });
+      } else {
+        res.status(401).json({ message: "Not Authorized!" });
+      }
     });
   }
 );
@@ -110,10 +117,15 @@ router.get("/:id", (req, res, next) => {
 });
 
 router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  });
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(
+    (result) => {
+      if (result.n > 0) {
+        res.status(200).json({ message: "Post deleted!" });
+      } else {
+        res.status(401).json({ message: "Not Authorized!" });
+      }
+    }
+  );
 });
 
 module.exports = router;
